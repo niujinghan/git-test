720f2d7aafbc2d613eabb5e43abb86a0b3989ecf
Niuniu <njh391020@163.com>
2024-08-03 15:50:48 +0800

    hot-fix test

--- src/main/java/com/njh/git/GitTest.java
+++ src/main/java/com/njh/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello njh2");
         System.out.println("hello njh3");
         System.out.println("hello njh-hot-fix1");
+        System.out.println("hot-fix test");
     }
 }
