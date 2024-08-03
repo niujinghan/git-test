54965b5a12d40351d587b3094e49a438c6bd03e7
Niuniu <njh391020@163.com>
2024-08-03 13:50:35 +0800

    third commit

--- src/main/java/com/njh/git/GitTest.java
+++ src/main/java/com/njh/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello njh");
         System.out.println("hello njh2");
+        System.out.println("hello njh3");
     }
 }
